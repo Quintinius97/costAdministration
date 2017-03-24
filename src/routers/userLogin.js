ff3310ecb850e77f09d7fbc95bff0fdc5936d0d1
Tom Richter <tom.richter@hpe.com>
2017-03-24 11:24:58 +0100

    removed sql and added bodyparser dummy

--- src/routers/userLogin.js
+++ src/routers/userLogin.js
@@ -1,6 +1,8 @@
 module.exports=function(app, route) {
   app.route(route)
   .post(function(req, res) {
+    let userName=req.body.userName;
+    let password=req.body.password;
     // TODO
   });
 };
