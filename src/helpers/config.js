9bf796119a27f3ca8f5c25a359667fec9e201ded
Buckwich <s.steinruecken@gmx.net>
2017-03-27 13:44:54 +0200

    corrected package

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,23 +1,25 @@
 'use strict';
 module.exports = function(path, obj) {
-  if (!path.endsWith('.json')) {
+  if(!path.endsWith('.json')) {
     path = path + '.json';
   }
 
   const fs = require('fs');
   let data = fs.readFileSync(path, 'utf8', function(err, data) {
-    if (err) {
+    if(err) {
       return console.error(err);
     }
   });
 
   let jsonObj = JSON.parse(data);
   let objs = obj.split('.');
-  for (let entity in objs) {
-    for (let i in jsonObj) {
-      if (i == objs[entity]) {
-        jsonObj = jsonObj[i];
-        break;
+  for(let entity in objs) {
+    if(Object.prototype.hasOwnProperty.call(objs, entity)) {
+      for(let i in jsonObj) {
+        if(i == objs[entity]) {
+          jsonObj = jsonObj[i];
+          break;
+        }
       }
     }
   }
