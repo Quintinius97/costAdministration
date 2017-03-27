016d24a5e32a6ab1d8d5d587293dd9cb3856247b
Chris <christof@weinreich-edv.de>
2017-03-27 18:08:05 +0200

    DB

--- src/dbConnector.js
+++ src/dbConnector.js
@@ -1,5 +1,24 @@
-const config = require('./helpers/config');
 const noSql = require('nosql');
 let categoryDB = noSql.load('./data/category.nosql');
 let costDB = noSql.load('./data/cost.nosql');
 let userDB = noSql.load('./data/user.nosql');
+
+module.exports.insert = function (db, JsonObj) {
+  switch (db) {
+    case 'category':
+        console.log(JsonObj);
+        console.log(JsonObj['id']);
+        categoryDB.insert(JsonObj, true).where('id',JsonObj['id']);
+
+      break;
+    case 'cost':
+
+      break;
+    case 'user':
+
+      break;
+    default:
+
+  }
+  // body...
+};
