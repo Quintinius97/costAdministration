a23f81fc888fe38ba909a3d53ce592bff7163030
Chris <christof@weinreich-edv.de>
2017-03-27 17:17:26 +0200

    keine DB config

--- src/dbConnector.js
+++ src/dbConnector.js
@@ -1,3 +1,5 @@
 const config = require('./helpers/config');
 const noSql = require('nosql');
-let userDB = noSql.load(config('./config/dbConfig', 'user'));
+let categoryDB = noSql.load('./data/category.nosql');
+let costDB = noSql.load('./data/cost.nosql');
+let userDB = noSql.load('./data/user.nosql');
