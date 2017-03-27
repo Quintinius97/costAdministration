90b42fddf3f7e03c09ae3553cf3cf4afec3324a8
Buckwich <s.steinruecken@gmx.net>
2017-03-27 12:30:31 +0200

    reformat

--- src/routers/costs.js
+++ src/routers/costs.js
@@ -1,12 +1,12 @@
-module.exports=function(app, route) {
-    app.route(route)
-      .get(function(req, res) {
-        // TODO
-      })
-      .post(function(req, res) {
-        // TODO
-      })
-      .delete(function(req, res) {
-        // TODO
-      });
+module.exports = function(app, route) {
+  app.route(route)
+    .get(function(req, res) {
+      // TODO
+    })
+    .post(function(req, res) {
+      // TODO
+    })
+    .delete(function(req, res) {
+      // TODO
+    });
 };
--- src/routers/userLogin.js
+++ src/routers/userLogin.js
@@ -1,8 +1,8 @@
-module.exports=function(app, route) {
+module.exports = function(app, route) {
   app.route(route)
-  .post(function(req, res) {
-    userName=req.body.userName;
-    password=req.body.password;
-    // TODO
-  });
+    .post(function(req, res) {
+      userName = req.body.userName;
+      password = req.body.password;
+      // TODO
+    });
 };
