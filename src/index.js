59f347d5fa56bc71f597a3b584e02878f4efddb4
Chris <christof@weinreich-edv.de>
2017-05-30 17:07:09 +0200

    rename console log

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const app = express();
 require('./router')(app);
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('costAdministration API listening on port 3000!');
 });
