dae3a087bf3c257298aa476692edd5b2edebf14e
Tom Richter <tom.richter@hpe.com>
2017-06-01 13:36:14 +0200

    category all. JWT fix

--- src/routers/categoryAll.js
+++ src/routers/categoryAll.js
@@ -5,6 +5,9 @@ const jwt = require('../helpers/jwt');
 module.exports = function(app, route) {
   app.route(route)
   .get(bodyParser, function(req, res) {
+    if(req.get('authorization')===undefined || req.get('authorization')===null) {
+      return res.status(401).send({error: 'Authorization required'});
+    }
     // Connect JWT
     jwt.connect(req.get('authorization'), function(err, decoded) {
       if(err || decoded === null || decoded === undefined) {
